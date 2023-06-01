7d512958b5ffa3ab9734f93884353c6c37fca22a
ClioP <86557689+ClioP@users.noreply.github.com>
2023-06-01 16:11:24 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,19 +49,20 @@ class footerTemplate extends HTMLElement {
 document.addEventListener('DOMContentLoaded', () => {
 	customElements.define('header-template', headerTemplate);
 	customElements.define('footer-template', footerTemplate);
+
+		// Add "active" class when menu item matches current page.
+	document.querySelectorAll('.menu-item').forEach((aciveItem) => {
+		if (aciveItem.href === window.location.href) {
+			aciveItem.classList.add('active');
+			aciveItem.setAttribute('aria-current', 'page');
+		}
+	});
+
+
+	// Script to dynamically get the current year.
+	const date = new Date();
+	let currentYear = date.getFullYear();
+	document.getElementById('current-year').innerHTML = currentYear;
 });
 
 
-// Add "active" class when menu item matches current page.
-document.querySelectorAll('.menu-item').forEach((aciveItem) => {
-  if (aciveItem.href === window.location.href) {
-    aciveItem.classList.add('active');
-    aciveItem.setAttribute('aria-current', 'page');
-  }
-});
-
-
-// Script to dynamically get the current year.
-const date = new Date();
-let currentYear = date.getFullYear();
-document.getElementById("current-year").innerHTML = currentYear;
